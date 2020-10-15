e3f9d3832b3320c46c0877d2e54d95c278b7e1ed
LAVADPR <lavadpr@oocl.com>
2020-10-15 21:58:34 +0800

    [TDD Basic] [Prince] Test - Execute multiple command

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -154,11 +154,23 @@ class MarsRoverTest {
     void should_CommandNotDefinedException_when_getRoverPosition_given_movement_F() throws MarsRover.CommandNotDefinedException {
         //given
         String movement = "F";
-        MarsRover marsRover = new MarsRover(0,0,"W");
+        MarsRover marsRover = new MarsRover(0,0,"N");
         //when
         //then
         assertThrows(MarsRover.CommandNotDefinedException.class, () -> {
             marsRover.getRoverPosition(movement);
         });
+    }
+    @Test
+    void should_x_n1_y_1_direction_N_when_getRoverPosition_given_x_0_y_0_direction_N_movement_MLMR() throws MarsRover.CommandNotDefinedException {
+        //given
+        String movement = "MLMR";
+        MarsRover marsRover = new MarsRover(0,0,"N");
+        //when
+        marsRover.getRoverPosition(movement);
+        //then
+        assertEquals(-1, marsRover.getX());
+        assertEquals(1, marsRover.getY());
+        assertEquals("N", marsRover.getDirection());
     }
 }
