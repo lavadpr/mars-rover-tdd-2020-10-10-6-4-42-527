31c95dced561dbb73a093eedac59b481a96a576d
LAVADPR <lavadpr@oocl.com>
2020-10-15 20:13:07 +0800

    [TDD Basic] [Prince] Test - Execute command R with given location and heading W

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -137,4 +137,16 @@ class MarsRoverTest {
         assertEquals(0, marsRover.getY());
         assertEquals("S", marsRover.getDirection());
     }
+    @Test
+    void should_x_0_y_0_direction_N_when_getRoverPosition_given_x_0_y_0_direction_W_movement_R() {
+        //given
+        String movement = "R";
+        MarsRover marsRover = new MarsRover(0,0,"W");
+        //when
+        marsRover.getRoverPosition(movement);
+        //then
+        assertEquals(0, marsRover.getX());
+        assertEquals(0, marsRover.getY());
+        assertEquals("N", marsRover.getDirection());
+    }
 }
